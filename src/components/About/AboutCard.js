2313e858287029fd3904c3332ce75b471d7fb44f
Loveneet Singh Chahal <chahal.loveneet@gmail.com>
2024-08-02 16:25:55 +0530

    updated files

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,17 +8,20 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Loveneet Chahal </span>
+            Hi Everyone, I am <span className="purple"> Loveneet Chahal </span>
             from <span className="purple"> Punjab, India.</span>
             <br />
             I am currently employed as a Senior Software Developer and Solutions Architect at Evolvous.
             <br />
-            I have completed Integrated B-Tech in Information Technology from PTU Jalandhar.
+            I have completed B-Tech in Information Technology from PTU Jalandhar (2009 - 2013).
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
+            <li className="about-activity">
+              <ImPointRight /> Surfing Internet
+            </li>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
@@ -28,10 +31,16 @@ function AboutCard() {
             <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing 
+            </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Don't hate if you can't Love!"{" "}
           </p>
           <footer className="blockquote-footer">Loveneet</footer>
         </blockquote>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">LOVENEET CHAHAL</strong>
+                <strong className="main-name"> LOVENEET CHAHAL </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
